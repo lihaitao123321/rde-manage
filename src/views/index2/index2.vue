715c9aba1ca543f073e2f468984eab013d9daebd
李海涛 <lihaitao@lihaitaodeMacBook-Pro.local>
2019-08-04 20:32:29 +0800

    接口对接

--- src/views/index2/index2.vue
+++ src/views/index2/index2.vue
@@ -1,34 +1,31 @@
+<template>
+    <baidu-map class="baidu" :center="center" :zoom="zoom" @ready="ready" ak="bLH7rG3fQ75DhH6dLkjhQbD7LV3rsmM7"></baidu-map>
+</template>
+<script>
+    import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+    export default {
+        components: {
+            BaiduMap
+        },
+        data () {
+            return {
+                center: {lng: 0, lat: 0},
+                zoom: 3
+            }
+        },
+        methods: {
+            ready ({BMap, map}) {
+                console.log(BMap, map)
+                this.center.lng = 116.404
+                this.center.lat = 39.915
+                this.zoom = 15
+            }
+        }
+    }
+</script>
 <style>
-    #allmap{
+    .baidu{
         width: 100%;
         height: 100%;
     }
 </style>
-<template>
-    <div id="allmap">
-
-    </div>
-</template>
-
-<script>
-    import { VChart, VGuide, VLine, VArea, VScale } from 'vux'
-
-    export default {
-        components: {
-            VChart,
-            VGuide,
-            VArea,
-            VLine,
-            VScale
-        },
-        data () {
-            return {
-
-            }
-        },
-        mounted(){
-
-        },
-    }
-</script>
-
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
 module.exports = {
     outputDir: 'D:\\project\\rde-cordova\\www\\',
     publicPath: './',
+    devServer: {
+        proxy: {
+            'organ/api': {
+                target: 'http://106.12.37.142/'
+            }
+        }
+    },
     configureWebpack: config => {
         require('vux-loader').merge(config, {
             options: {},
             plugins: [
                 'vux-ui',
-
                 {
                     name: 'less-theme',
                     path: './src/css/theme.less'
