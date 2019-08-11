720236b4ddbebfb1b4e17d2d72a06a031a35bf5f
charles <charles.yiako.com>
2019-08-11 21:25:30 +0800

    个人中心

---
+++ src/views/index5/language.vue
@@ -0,0 +1,34 @@
+<template>
+        <div>
+            <div class="language">
+                <XHeader :left-options="{backText: ''}" title="语言类型"></XHeader>
+                <group >
+                    <radio  :options="options" v-model="value"></radio>
+                </group>
+            </div>
+        </div>
+    </template>
+      
+      <script>
+      import { XHeader,Radio,Group } from "vux";
+      
+      export default {
+        components: {
+            Radio,
+            Group,
+            XHeader
+        },
+        data() {
+          return {
+            options:['简体中文','English','日本語'],
+            value:'简体中文'
+          };
+        }
+      };
+      </script>
+    
+      <style scoped>
+         /deep/ .weui-cell {
+             padding: 16px 15px;
+         }
+      </style>
